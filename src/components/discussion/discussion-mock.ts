10aef0ecdf4eae770766cac9bd2df093f9083f22
Atif Rafique <atif.rafique@ceative.co.uk>
2023-07-04 12:01:36 +0500

    #comment discussion added

---
+++ src/components/discussion/discussion-mock.ts
@@ -0,0 +1,11 @@
+export const DISCUSSION_DATA = [
+  {
+    id: "01",
+    heading: "1) SPA",
+    description: `SPA stands for Single-Page Application. It is a type of web application that delivers a seamless user experience by dynamically updating the content on a single web page, instead of loading entire new pages when the user interacts with the application.
+In traditional web applications, each interaction, such as clicking a link or submitting a form, typically triggers a full page reload. This approach can result in slower and less responsive user interfaces. In contrast, SPAs load the initial web page and then dynamically update the content as the user interacts with the application, without the need for full page reloads.
+SPAs achieve this by using JavaScript frameworks or libraries, such as React, Angular, or Vue.js, to handle the rendering and updating of the content. The initial HTML, CSS, and JavaScript are loaded when the SPA is first accessed, and subsequent interactions trigger requests for data or additional views that are then rendered dynamically.
+Some benefits of SPAs include improved performance, as only the necessary data or views are loaded, and a more interactive and fluid user experience similar to that of a desktop application. However, SPAs also have some considerations, such as increased complexity in managing state and SEO (Search Engine Optimization) challenges, which may require additional techniques to ensure search engines can properly index the content.
+Overall, SPAs have become popular for building modern web applications, particularly those that require rich interactivity and responsiveness.`,
+  },
+];
--- src/components/discussion/discussion.tsx
+++ src/components/discussion/discussion.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Collapse } from 'antd';
+import { DISCUSSION_DATA } from './discussion-mock';
+
+
+const { Panel } = Collapse;
+
 
 const DiscussionComponent = () => {
     return (
-        <div>DiscussionComponent</div>
+        <div>
+
+            <h2>Discussion</h2>
+
+            <Collapse accordion>
+                {DISCUSSION_DATA?.map((singleProgram: any) => (
+                    <Panel header={singleProgram?.heading} key={singleProgram?.id}>
+                        <p><strong>Statement:</strong> {singleProgram?.description}</p>
+                    </Panel>
+                ))}
+            </Collapse>
+        </div>
     )
 }
 
